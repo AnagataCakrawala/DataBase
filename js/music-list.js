b5a1547d3a6c4bcc5350c7de524d3f210b8c18e9
Anagata-Cakrawala <88611212+Stevens0856@users.noreply.github.com>
2024-03-10 21:25:45 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,224 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Apollo On The Run",
+    artist: "Georgina Black",
+    img: "music-0",
+    src: "music-1"
+  },
+  {
+    name: "Dont deelevate elevate others to happiness",
+    artist: "sharonista Remix",
+    img: "music-0",
+    src: "music-2"
+  },
+  {
+    name: "Infinity (slowed)",
+    artist: "Jaymes Young",
+    img: "music-0",
+    src: "music-3"
+  },
+  {
+    name: "Linkin Park - In The End",
+    artist: "Mellen Gi & Tommee Profitt Remix",
+    img: "music-0",
+    src: "music-4"
+  },
+  {
+    name: "Lost Sky - Fearless Pt. II",
+    artist: "artist ft. Chris Linton",
+    img: "music-0",
+    src: "music-5"
+  },
+  {
+    name: "Nightcore -  End Of Time x Lonely World",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-6"
+  },
+  {
+    name: "Nightcore - After Dark",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-7"
+  },
+  {
+    name: "Nightcore - Break The Silence",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-8"
+  },
+  {
+    name: "Nightcore - Diamond Heart",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-9"
+  },
+  {
+    name: "Nightcore - Love Story",
+    artist: "Indila",
+    img: "music-0",
+    src: "music-10"
+  },
+  {
+    name: "Nightcore - Legends Never Die",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-11"
+  },
+  {
+    name: "Nightcore - Love Story",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-12"
+  },
+  {
+    name: "Nightcore - Minefields",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-13"
+  },
+  {
+    name: "Nightcore - Mortals",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-14"
+  },
+  {
+    name: "Nightcore - The Night ",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-15"
+  },
+  {
+    name: "Nightcore - Al final",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-16"
+  },
+  {
+    name: "Nightcore - can we kiss forever x you broke me first",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-17"
+  },
+  {
+    name: "Nightcore - End of Time ✗ Unity Switching vocals",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-18"
+  },
+  {
+    name: "Nightcore - lovely",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-19"
+  },
+  {
+    name: "Rootkit - Only Knew",
+    artist: "feat. Nevve",
+    img: "music-0",
+    src: "music-20"
+  },
+  {
+    name: "Nightcore - Royalty",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-21"
+  },
+  {
+    name: "Alan Walker - Hello World",
+    artist: "feat. Torine",
+    img: "music-0",
+    src: "music-22"
+  },
+  {
+    name: "Alan Walker - Unity [Slowed Tiktok Version]",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-23"
+  },
+  {
+    name: "Baby don't Go",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-24"
+  },
+  {
+    name: "Bloody Mary - Instrumental Slowed",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-25"
+  },
+  {
+    name: "Nightcore - I Don't Wanna Be Sad Forever",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-26"
+  },
+  {
+    name: "Nightcore - Play x Faded x Unity x Alone",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-27"
+  },
+  {
+    name: "Stronger",
+    artist: "TheFatRat",
+    img: "music-0",
+    src: "music-28"
+  },
+  {
+    name: "Nightcore - Thunder",
+    artist: "Gabry Ponte,LUM!X,Prezioso",
+    img: "music-0",
+    src: "music-29"
+  },
+  {
+    name: "Nightcore - Faded x Darkside x Lily x Lost Control",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-30"
+  },
+  {
+    name: "Nightcore - Play x Alone x Darkside x Ignite",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-31"
+  },
+  {
+    name: "everythings works out",
+    artist: "artist unknown",
+    img: "music-0",
+    src: "music-32"
+  },
+  {
+    name: " KARENA KAMU - SLOWED REVERB",
+    artist: "GEISHA",
+    img: "music-0",
+    src: "music-33"
+  },
+  {
+    name: "Karena Kamu - (SPEED UP REVERB) [TIKTOK VERSION]",
+    artist: "GEISHA",
+    img: "music-0",
+    src: "music-34"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
